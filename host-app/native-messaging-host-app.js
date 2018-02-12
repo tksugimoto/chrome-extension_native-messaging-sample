40711c426f536b6687f38ba645d10a889e8534ad
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-02-12 23:33:39 +0900

    feat: host appとのデータのやりとり処理を実装

--- host-app/native-messaging-host-app.js
+++ host-app/native-messaging-host-app.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const util = require('util');
 
 const logPath = './out.log';
 
@@ -11,16 +12,46 @@ ${process.argv.map((v, i) => `${i}: ${v}`).join('\n')}
 
 fs.writeFileSync(logPath, logText);
 
+const lengthBytes = 4;
 
-process.stdin.pipe(fs.createWriteStream(logPath, {
-    flags: 'a',
-}));
-
-process.stdin.on('data', data => {
-    if (data.toString().trim() === 'quit') process.exit();
+process.stdin.once('data', _data => {
+    const data = new Buffer(_data);
+    const lengthBuffer = data.slice(0, lengthBytes);
+    const bodyBuffer = data.slice(lengthBytes);
+    const bodyLength = lengthBuffer.reduceRight((acc, cur) => acc * 256 + cur);
+    log(`data.length: ${data.length}`);
+    log(`bodyLength: ${bodyLength}`);
+    if (bodyBuffer.length !== bodyLength) {
+        // TODO エラー処理
+        return process.exit(1);
+    }
+    const input = JSON.parse(bodyBuffer.toString());
+    log(`[input]\n${JSON.stringify(input, null, '\t')}`);
+    send({
+        res: 'hi!',
+        time: new Date(),
+        inputValue: input.value,
+        nodejs: process.argv[0],
+    });
+    log(`[raw data.toJSON()]\n${util.inspect(data.toJSON(), {
+        maxArrayLength: 30,
+        showHidden: true,
+    })}`);
 });
 
+const send = (messageObject) => {
+    const message = JSON.stringify(messageObject);
+    const messageBuffer = Buffer.from(message);
+    const length = messageBuffer.length;
+    const lengthBuffer = Buffer.alloc(lengthBytes);
+    lengthBuffer.writeUIntLE(length, 0, lengthBytes);
 
-process.stdin.once('data', data => {
-    process.stdout.write(data);
-});
+    process.stdout.write(lengthBuffer);
+    process.stdout.write(messageBuffer);
+};
+
+const log = str => {
+    fs.writeFileSync(logPath, `${str}\n`, {
+        flag: 'a',
+    });
+};
