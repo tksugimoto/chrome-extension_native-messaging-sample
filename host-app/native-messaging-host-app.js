7d921c17ff44fc63041e3381c72ebc9672119de4
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-12-01 23:31:27 +0900

    feat: host-app: 標準入力をログファイルに出力

--- host-app/native-messaging-host-app.js
+++ host-app/native-messaging-host-app.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+
+const logPath = './out.log';
+
 const logText = `${new Date().toLocaleString()}
 ${process.argv.map((v, i) => `${i}: ${v}`).join('\n')}
+=========================
 `;
-fs.writeFileSync('./out.log', logText);
+
+fs.writeFileSync(logPath, logText);
+
+
+process.stdin.pipe(fs.createWriteStream(logPath, {
+    flags: 'a',
+}));
+
+process.stdin.on('data', data => {
+    if (data.toString().trim() === 'quit') process.exit();
+});
