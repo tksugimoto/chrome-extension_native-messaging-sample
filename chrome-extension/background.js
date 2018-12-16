85b2b18eebd6840c56f78bea82daae9969dc0d45
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-12-17 00:38:16 +0900

    feat: ローカルファイルリンク(file:///) をExplorerで開く機能を追加

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,6 +1,20 @@
+import common from './common.js';
 
 chrome.runtime.onInstalled.addListener(details => {
 	if (details.reason === 'install') {
 		chrome.runtime.openOptionsPage();
+	}
+});
+
+chrome.runtime.onMessage.addListener((message) => {
+	if (message.method === 'open-local-file-by-explorer') {
+		const localFileUrl = message.localFileUrl;
+		const filePath = decodeURI(localFileUrl).replace(/^file:\/\/\//, '').replace(/\//g, '\\');
+		const messageToNative = {
+			filePath,
+		};
+		chrome.runtime.sendNativeMessage(common.applicationName, messageToNative, response => {
+			console.info({response});
+		});
 	}
 });
